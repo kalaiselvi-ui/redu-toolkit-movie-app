ac9536218bff6e1d8f55ba328c103b5665e25a29
kalaiselvi-ui <kalaichandran369@gmail.com>
2024-12-11 12:03:56 +0530

    moviedetailscard component created based on id

--- src/components/MovieDetail/MovieDetail.js
+++ src/components/MovieDetail/MovieDetail.js
@@ -1,7 +1,27 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import "./MovieDetail.scss";
 
 const MovieDetail = () => {
-  return <div>MovieDetail</div>;
+  const { imdbID } = useParams(); // Extract imdbID from the route
+
+  // Select the movie data from Redux store
+  const movie = useSelector((state) =>
+    state.movies.movies.find((movie) => movie.imdbID === imdbID)
+  );
+
+  if (!movie) {
+    return <div>Movie not found!</div>; // Show error message if the movie is not found
+  }
+
+  return (
+    <div className="movie-details">
+      <h2>{movie.Title}</h2>
+      <img src={movie.Poster} alt={movie.Title} />
+      <p>{movie.Year}</p>
+    </div>
+  );
 };
 
 export default MovieDetail;
